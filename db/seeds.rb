fb8e61aec93e1d590e76bbb69884222ef4344212
sandy guillot <sandyguillot@gmail.com>
2019-05-22 22:17:53 +0200

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,12 @@
 require "open-uri"
 require "json"
+
 result_hash = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
 ingredients = []
 result_hash["drinks"].each do |ingredient|
   ingredients << ingredient["strIngredient1"]
 end
-ingredients.each do |ingredient|
-  Ingredient.create!(name: ingredient)
-endrequire "open-uri"
-require "json"
-result_hash = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
-ingredients = []
-result_hash["drinks"].each do |ingredient|
-  ingredients << ingredient["strIngredient1"]
-end
+
 ingredients.each do |ingredient|
   Ingredient.create!(name: ingredient)
 end
