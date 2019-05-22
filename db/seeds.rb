8cc0e793c2e4b07ca19e62fccd5138ac481bd5e7
sandy guillot <sandyguillot@gmail.com>
2019-05-22 20:55:20 +0200

    commit cocktails db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require "open-uri"
+require "json"
+result_hash = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+ingredients = []
+result_hash["drinks"].each do |ingredient|
+  ingredients << ingredient["strIngredient1"]
+end
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient)
+end
